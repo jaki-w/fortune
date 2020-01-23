fbea45b00c9f10c817b3c9aa772f86dc5903fa05
Student <example@example.com>
2020-01-22 16:41:46 -0800

    create function to collect occurances values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-	$("#go").click(function(event) {
+	$("#prophecy").click(function(event) {
 		event.preventDefault();
 
-		var bigNumber = parseInt($("#countUp").val());
-	
-		var smallNumber = parseInt($("#multiple").val());
-	
-		var i = smallNumber;
-		
-		for(var i = smallNumber; i <= bigNumber; i += smallNumber) {
-			$("#results").append(i + ", ");
-    
-    //   if (bigNumber === " " || smallNumber === " ") {
-    //     alert("You're missing a number!");
-    //   } else if (isNaN(bigNumber) || isNaN(smallNumber) {
-    //     alert("please enter a NUMBER");
-    //   } else {
-    //     $("results").append(i + ", ");
-    // }
-	});
+    var goodOccurances = [];
+    var badOccurances = [];
+    $("input:checkbox[name=unlucky]:checked").each(function() {
+      var unluckyOccurances = parseInt($("input:checkbox[name=unlucky]:checked").val());
+     
+    });
+    $("input:checkbox[name=lucky]:checked").each(function() {
+      var luckyOccurances = parseInt($("input:checkbox[name=lucky]:checked").val());
+     
+    });
+    alert(totalGood);
+
+
+
+  });
 });
